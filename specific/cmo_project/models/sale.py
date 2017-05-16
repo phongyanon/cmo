7c7e84a222427ceebc34a7e20efd189093518b34
phongyanon <phongyanon@gmail.com>
2017-05-16 11:53:07 +0700

    [IMP] group, custom_group fields in sale_layout

--- specific/cmo_project/models/sale.py
+++ specific/cmo_project/models/sale.py
@@ -6,6 +6,13 @@ from openerp import fields, models, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    project_number = fields.Char(
+        string='Project Code',
+        readonly=True,
+        states={'close': [('readonly', True)]},
+        # compute="_get_project_number",
+        copy=False,
+    )
     # version = fields.Integer(
     #     string='Version',
     #     size=4,
@@ -15,11 +22,32 @@ class SaleOrder(models.Model):
         string='Version',
     )
 
+    # @api.multi
+    # def _get_project_number(self):
+    #     print(">>>>>>>>>>>>>>>> Get project Number")
+    #     for quote in self:
+    #         print(quote)
+    #         if quote.project_id:
+    #             print(quote.project_id)
+    #             project = self.env['project.project'].search(analytic_account_id=quote.project_id)
+    #             self.write({'project_number':project.project_number})
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    order_lines_level = fields.Selection(
+    order_lines_group = fields.Selection(
+        [('before','Before Management Fee'),
+         ('manage_fee','Management and Operation Fee'),
+        ],
+        string='Group',
+    )
+
+    sale_layout_custom_group_id = fields.Many2one(
+        'sale_layout.custom_group',
+        string='Custom Group',
+    )
+
+    section_code = fields.Selection(
         [('A', 'A'),
          ('B', 'B'),
          ('C', 'C'),
@@ -27,6 +55,18 @@ class SaleOrderLine(models.Model):
          ('E', 'E'),
          ('F', 'F'),
         ],
-        string='Level',
-        states={'close': [('readonly', True)]},
+        string='Section Code',
     )
+
+
+class SaleLayoutCustomGroup(models.Model):
+    _name = 'sale_layout.custom_group'
+
+    name = fields.Char(
+        string="Name",
+        required=True,
+    )
+
+    _sql_constraints = [
+        ('name_uniq', 'UNIQUE(name)', 'Name must be unique!'),
+    ]
