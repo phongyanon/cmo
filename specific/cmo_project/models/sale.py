e3140082c6e96990bdb7acb084c3fb90acd5f879
phongyanon <phongyanon@gmail.com>
2017-05-17 11:23:06 +0700

    [IMP] show project number in quotation

--- specific/cmo_project/models/sale.py
+++ specific/cmo_project/models/sale.py
@@ -9,8 +9,6 @@ class SaleOrder(models.Model):
     project_number = fields.Char(
         string='Project Code',
         readonly=True,
-        states={'close': [('readonly', True)]},
-        # compute="_get_project_number",
         copy=False,
     )
     # version = fields.Integer(
@@ -18,19 +16,41 @@ class SaleOrder(models.Model):
     #     size=4,
     #     default=None,
     # )
+    project_related_id = fields.Many2one(
+        'project.project',
+        string='Project',
+    )
+
     version = fields.Char(
         string='Version',
     )
 
-    # @api.multi
-    # def _get_project_number(self):
-    #     print(">>>>>>>>>>>>>>>> Get project Number")
-    #     for quote in self:
-    #         print(quote)
-    #         if quote.project_id:
-    #             print(quote.project_id)
-    #             project = self.env['project.project'].search(analytic_account_id=quote.project_id)
-    #             self.write({'project_number':project.project_number})
+    @api.onchange('project_related_id')
+    def _get_project_number(self):
+        project = self.env['project.project'].browse(self.project_related_id.id)
+        print(">>>>>>>>>>>>>>>>>>>>>", self.project_related_id.id, project.project_number)
+        self.project_id = project.analytic_account_id.id
+        self.project_number = project.project_number
+
+    @api.multi
+    def write(self, vals):
+        self.ensure_one()
+        if 'project_related_id' in vals:
+            project = self.env['project.project'].browse(vals['project_related_id'])
+            self.write({
+                'project_id' :  project.analytic_account_id.id,
+                'project_number' : project.project_number,
+            })
+        return super(SaleOrder, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        if 'project_related_id' in vals:
+            project = self.env['project.project'].browse(vals['project_related_id'])
+            vals['project_id'] = project.analytic_account_id.id
+            vals['project_number'] = project.project_number
+        return super(SaleOrder, self).create(vals)
+
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
@@ -40,6 +60,7 @@ class SaleOrderLine(models.Model):
          ('manage_fee','Management and Operation Fee'),
         ],
         string='Group',
+        default='before',
     )
 
     sale_layout_custom_group_id = fields.Many2one(
