94d3787b17425adba4c604c7f3ef21d0f5b8b344
phongyanon <phongyanon@gmail.com>
2017-05-29 16:59:18 +0700

    [ADD] combine product to sale

---
+++ specific/cmo_sale/models/product.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from openerp import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    sale_layout_cat_id = fields.Many2one(
+        'sale_layout.category',
+        string='Section',
+    )
