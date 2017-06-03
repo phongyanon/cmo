649f0b0f45ce0d575e4f547923a8f233a613dbba
phongyanon <phongyanon@gmail.com>
2017-06-03 15:02:41 +0700

    [IMP] use compute instead of inverse

--- specific/cmo_sale/models/sale.py
+++ specific/cmo_sale/models/sale.py
@@ -11,13 +11,10 @@ class SaleConvenantDescription(models.Model):
     )
     description = fields.Text(
         string='Description',
+        translate=True,
     )
     active = fields.Boolean(
         string='Active',
-        default=True,
-    )
-    is_eng = fields.Boolean(
-        string='Is English',
         default=True,
     )
 
@@ -32,12 +29,12 @@ class SaleOrder(models.Model):
     project_number = fields.Char(
         string='Project Code',
         readonly=True,
+        compute='_compute_project_number',
         copy=False,
     )
     project_related_id = fields.Many2one(
         'project.project',
         string='Project',
-        inverse='_compute_project_related_id',
         states={'done': [('readonly', True)]},
     )
     event_date_description = fields.Char(
@@ -58,39 +55,12 @@ class SaleOrder(models.Model):
         string='Payment Term',
         states={'done': [('readonly', True)]},
     )
-    convenant_description_eng = fields.Text(
-        string='Convenant',
-        states={'done': [('readonly', True)]},
-        default=lambda self: self._get_eng_convenant(),
-    )
-    convenant_description_th = fields.Text(
-        string=u'เงื่อนไขการยกเลิก',
-        states={'done': [('readonly', True)]},
-        default=lambda self: self._get_th_convenant(),
-    )
-    discount_type = fields.Selection(
-        [('percent', 'Percentage'),
-         ('amount', 'Amount')],
-         inverse='_compute_discount_rate',
-         string='Discount Type',
-         states={'done': [('readonly', True)]},
-    )
-    discount_rate = fields.Float(
-        string='Discount Rate',
-        inverse='_compute_discount_rate',
-        store=True,
+    convenant_description = fields.Text(
+        string='Covenant',
+        translate=True,
+        default=lambda self: self._default_covenant(),
         states={'done': [('readonly', True)]},
     )
-    amount_discount = fields.Float(
-        string="Discount",
-        compute='_compute_amount_discount',
-        readonly=True,
-    )
-
-    @api.depends('amount_untaxed')
-    def _compute_amount_discount(self):
-        total = sum(self.order_line.mapped('price_unit'))
-        self.amount_discount = total - self.amount_untaxed
 
     @api.multi
     @api.depends('amount_before_management_fee')
@@ -100,20 +70,6 @@ class SaleOrder(models.Model):
             ).mapped('price_unit'))
         self.amount_before_management_fee = total
 
-    @api.depends('discount_type', 'discount_rate', 'order_line')
-    def _compute_discount_rate(self):
-        lines = self.order_line
-        if not self.discount_type:
-            return
-        percent = 0
-        if self.discount_type == 'percent':
-            percent = self.discount_rate
-        elif self.discount_type == 'amount':
-            total = sum(self.order_line.mapped('price_unit'))
-            percent = (self.discount_rate * 100.0) / total
-        for line in lines: # TODO: refactor iterator
-            line.discount = percent
-
     @api.onchange('project_related_id')
     def _onchange_project_number(self):
         project = self.env['project.project']\
@@ -122,26 +78,17 @@ class SaleOrder(models.Model):
         self.project_number = project.project_number
 
     @api.multi
-    def _compute_project_related_id(self):
+    @api.depends('project_number', 'project_related_id')
+    def _compute_project_number(self):
         for quote in self:
             parent_project = self.env['project.project']\
                 .browse(quote.project_related_id.id)
             quote.project_id = parent_project.analytic_account_id.id
             quote.project_number = parent_project.project_number
 
-    @api.multi
-    def _get_eng_convenant(self):
-        convenants = self.env['sale.convenant.description'].search([
-            ['is_eng', '=', True],
-            ['active', '=', True],
-        ])
-        if convenants:
-            return convenants[0].description
-
     @api.model
-    def _get_th_convenant(self):
+    def _default_covenant(self):
         convenants = self.env['sale.convenant.description'].search([
-            ['is_eng', '=', False],
             ['active', '=', True],
         ])
         if convenants:
@@ -158,18 +105,19 @@ class SaleOrderLine(models.Model):
         string='Group',
         default='before',
     )
-
     sale_layout_custom_group_id = fields.Many2one(
         'sale_layout.custom_group',
         string='Custom Group',
     )
-
     sale_order_line_margin = fields.Float(
         string='Margin',
-        compute='_get_sale_order_line_margin',
+        compute='_compute_sale_order_line_margin',
         readonly=True,
     )
-
+    so_line_percent_margin = fields.Float(
+        string='Percentage',
+        compute='_compute_so_line_percent_margin',
+    )
     section_code = fields.Selection(
         [('A', 'A'),
          ('B', 'B'),
@@ -193,10 +141,23 @@ class SaleOrderLine(models.Model):
             'context': {'order_line_id': self.id, 'view_id': 'view_sale_management_fee',}
         }
 
+    @api.multi
     @api.onchange('price_unit', 'purchase_price')
-    def _get_sale_order_line_margin(self):
-        margin = self.price_unit - self.purchase_price
-        self.sale_order_line_margin = margin
+    def _compute_sale_order_line_margin(self):
+        for line in self:
+            margin = line.price_unit - line.purchase_price
+            line.sale_order_line_margin = margin
+
+    @api.multi
+    @api.onchange('price_unit', 'purchase_price')
+    def _compute_so_line_percent_margin(self):
+        for line in self:
+            margin = line.price_unit - line.purchase_price
+            if line.price_unit:
+                line.so_line_percent_margin = margin * 100.0 / line.price_unit
+            else:
+                line.so_line_percent_margin = 0.0
+
 
 class SaleLayoutCustomGroup(models.Model):
     _name = 'sale_layout.custom_group'
