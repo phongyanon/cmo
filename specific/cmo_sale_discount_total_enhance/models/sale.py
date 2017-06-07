2439fd1e471b2e773a4a73440261a4f0c57ea30d
phongyanon <phongyanon@gmail.com>
2017-06-07 14:26:44 +0700

    [FIX] update discount after write

--- specific/cmo_sale_discount_total_enhance/models/sale.py
+++ specific/cmo_sale_discount_total_enhance/models/sale.py
@@ -7,53 +7,16 @@ import openerp.addons.decimal_precision as dp
 class sale_order(models.Model):
     _inherit = 'sale.order'
 
-    discount_type = fields.Selection([
-        ('percent', 'Percentage'),
-        ('amount', 'Amount')
-        ],
-        string='Discount type',
-        readonly=True,
-        default='percent',
-        states={
-            'draft': [('readonly', False)],
-            'sent': [('readonly', False)]
-        },
-    )
-    discount_rate = fields.Float(
-        string='Discount Rate',
-        digits_compute=dp.get_precision('Account'),
-        readonly=True, states={
-            'draft': [('readonly', False)],
-            'sent': [('readonly', False)]
-        },
-    )
     amount_untaxed = fields.Float(
-        string='Untaxed Amount',
-        readonly=True,
-        compute='_amount_all',
-        track_visibility='always',
         store=False,
     )
     amount_tax = fields.Float(
-        string='Taxes',
-        readonly=True,
-        compute='_amount_all',
-        track_visibility='always',
         store=False,
     )
     amount_total = fields.Float(
-        string='Total',
-        readonly=True,
-        compute='_amount_all',
-        track_visibility='always',
         store=False,
     )
     amount_discount = fields.Float(
-        string='Discount',
-        readonly=True,
-        compute='_amount_all',
-        digits_compute=dp.get_precision('Account'),
-        track_visibility='always',
         store=False,
     )
     amount_before_discount = fields.Float(
@@ -65,12 +28,12 @@ class sale_order(models.Model):
     )
 
     @api.model
-    def create(self, vals):
-        res = super(sale_order, self).create(vals)
+    def calculate_discount(self, vals, order_id):
+        discount = 0
         if ('discount_rate' in vals) and ('order_line' in vals):
+            res = self.env['sale.order'].browse(order_id)
             if vals['discount_type'] == 'percent':
-                for line in res.order_line:
-                    line.write({'discount': vals['discount_rate']})
+                discount = vals['discount_rate']
             else:
                 amount = sum(res.order_line.mapped(
                     lambda r: r.product_uom_qty * r.price_unit)
@@ -81,8 +44,27 @@ class sale_order(models.Model):
                     discount = (discount_rate / total) * 100.0
                 else:
                     discount = discount_rate
-                for line in res.order_line:
-                    line.write({'discount': discount})
+            for line in res.order_line:
+                line.write({'discount': discount})
+
+    @api.model
+    def create(self, vals):
+        res = super(sale_order, self).create(vals)
+        self.calculate_discount(vals, res.id)
+        res._amount_all()
+        return res
+
+    @api.multi
+    def write(self, vals):
+        res = super(sale_order, self).write(vals)
+        for order in self:
+            if ('discount_rate' in vals) and ('order_line' in vals):
+                if 'discount_type' not in vals:
+                    vals['discount_type'] = order.discount_type
+                if 'discount_rate' not in vals:
+                    vals['discount_rate'] = order.discount_rate
+                self.calculate_discount(vals, order.id)
+                order._amount_all()
         return res
 
     @api.multi
@@ -98,7 +80,8 @@ class sale_order(models.Model):
                 amount_discount += (line.product_uom_qty *
                                     line.price_unit *
                                     line.discount) / 100
-                amount_before_discount += line.price_unit
+                amount_before_discount += (line.product_uom_qty *
+                                           line.price_unit)
             order.amount_untaxed = cur.round(amount_untaxed)
             order.amount_tax = cur.round(amount_tax)
             order.amount_discount = cur.round(amount_discount)
@@ -123,19 +106,19 @@ class sale_order(models.Model):
                     discount = order.discount_rate
                 for line in order.order_line:
                     line.discount = discount
-        self._amount_all()
+        self._amount_all
 
 
-class SaleOlinrderLine(models.Model):
+class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    sub_total_line = fields.Float(
+    price_subtotal_no_disco = fields.Float(
         string='Sub Total',
-        compute='_compute_sub_total_line',
+        compute='_compute_price_subtotal_no_disco',
     )
 
     @api.multi
     @api.depends('price_unit', 'product_uom_qty')
-    def _compute_sub_total_line(self):
+    def _compute_price_subtotal_no_disco(self):
         for line in self:
-            line.sub_total_line = line.price_unit * line.product_uom_qty
+            line.price_subtotal_no_disco = line.price_unit * line.product_uom_qty
