ec3d86b91f6c20f6900f45c4f63120fd81dc4c0e
phongyanon <phongyanon@gmail.com>
2017-06-12 11:00:53 +0700

    [ADD] sale split quote to many customer

---
+++ specific/cmo_sale_split_quote2many_order/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "CMO :: Sale Quotation split to many order",
+    'summary': "Sale Quotation split to many order for miltiple customer",
+    'author': "Ecosoft",
+    'website': "http://ecosoft.co.th",
+    'category': 'Tools',
+    "version": "1.0",
+    'depends': [
+        'sale',
+        'sale_split_quote2order',
+        'cmo_sale_discount_total_enhance',
+    ],
+    'data': [
+        'views/sale.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
