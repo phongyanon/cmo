53f258ba6f4063cbc8f80c61fa54a9104def234d
phongyanon <phongyanon@gmail.com>
2017-07-17 16:13:55 +0700

    [IMP] project relate invoice

--- specific/cmo_account/__openerp__.py
+++ specific/cmo_account/__openerp__.py
@@ -11,6 +11,7 @@
     "license": "AGPL-3",
     "depends": [
         'account',
+        'sale_layout',
     ],
     "data": [
         'views/product_view.xml',
--- specific/cmo_account/models/account.py
+++ specific/cmo_account/models/account.py
@@ -11,81 +11,72 @@ class AccountInvoice(models.Model):
         compute='_compute_quote_ref_id',
         readonly=True,
     )
-    quote_ref_number = fields.Char(
-        string='Quotation Number',
-        readonly=True,
-        compute='_compute_quote_ref_id',
-    )
-    quote_ref_date = fields.Datetime(
-        string='Quotation Date',
-        readonly=True,
-        compute='_compute_quote_ref_id',
-    )
-    quote_ref_event_date = fields.Char(
-        string='Event Date',
-        readonly=True,
-        compute='_compute_quote_ref_id',
-    )
-    quote_ref_venue = fields.Char(
-        string='Venue',
-        readonly=True,
-        compute='_compute_quote_ref_id',
-    )
-    project_ref_number = fields.Char(
-        string='Project Number',
-        readonly=True,
-        compute='_compute_quote_ref_id',
-    )
-    project_ref_name = fields.Char(
-        string='Project Name',
-        readonly=True,
-        compute='_compute_quote_ref_id',
-    )
     purchase_ref_id = fields.Many2one(
         'purchase.order',
         readonly=True,
-        compute='_compute_purchase_ref_id',
+        compute='_compute_quote_ref_id',
     )
     project_ref_id = fields.Many2one(
         'project.project',
         string='Project Ref.',
-        compute='_compute_project_ref_id',
+        compute='_compute_quote_ref_id',
+    )
+    quote_ref_number = fields.Char(
+        string='Quotation Number',
+        readonly=True,
+        related='quote_ref_id.name',
+    )
+    quote_ref_date = fields.Datetime(
+        string='Quotation Date',
+        readonly=True,
+        related='quote_ref_id.date_order',
+    )
+    quote_ref_event_date = fields.Char(
+        string='Event Date',
+        readonly=True,
+        related='quote_ref_id.event_date_description',
+    )
+    quote_ref_venue = fields.Char(
+        string='Venue',
+        readonly=True,
+        related='quote_ref_id.venue_description',
+    )
+    project_ref_number = fields.Char(
+        string='Project Number',
+        readonly=True,
+        related='project_ref_id.project_number',
+    )
+    project_ref_name = fields.Char(
+        string='Project Name',
+        readonly=True,
+        related='project_ref_id.name',
     )
 
     @api.multi
     @api.depends(
         'origin',
         'quote_ref_id',
-        'quote_ref_number',
-        'quote_ref_date',
-        'project_ref_number',
-        'project_ref_name',
-        'quote_ref_event_date',
-        'quote_ref_venue',
+        'purchase_ref_id',
+        'project_ref_id',
     )
     def _compute_quote_ref_id(self):
         for invoice in self:
-            quote_ref = self.env['sale.order'].search([
-                ('name', '=', invoice.origin)
-            ])
-            quote_ref = quote_ref.quote_id or False
-            if quote_ref:
-                invoice.quote_ref_id = quote_ref
-                invoice.quote_ref_number = quote_ref.name
-                invoice.quote_ref_date = quote_ref.date_order
-                invoice.quote_ref_event_date = quote_ref.event_date_description
-                invoice.quote_ref_venue = quote_ref.venue_description
-
-                project_ref = quote_ref.project_related_id or False
-                if project_ref:
-                    invoice.project_ref_number = project_ref.project_number
-                    invoice.project_ref_name = project_ref.name
-
-    # @api.multi
-    # @api.depends('project_ref_id')
-    # def _compute_project_ref_id(self):
-    #     for invoice in self:
-    #         origin_ref = self.env['sale.order'].search([
-    #             ('name', '=', invoice.origin)
-    #         ])
-    #         invoice.project_ref_id = origin_ref.
+            print('>>>>>>>>>>>>>>>>>>>>>', invoice._context.get('journal_type'))
+            if invoice._context.get('journal_type') == 'sale':
+                order_env = self.env['sale.order']
+                origin_ref = order_env.search([
+                    ('name', '=', invoice.origin)
+                ])
+                origin_ref = origin_ref.quote_id or False
+                if origin_ref:
+                    invoice.quote_ref_id = origin_ref
+                    project_ref = origin_ref.project_related_id or False
+                    if project_ref:
+                        invoice.project_ref_id = project_ref
+            elif invoice._context.get('journal_type') == 'purchase':
+                order_env = self.env['purchase.order']
+                origin_ref = order_env.search([
+                    ('name', '=', invoice.origin)
+                ])
+                invoice.purchase_ref_id = origin_ref
+                invoice.project_ref_id = origin_ref.project_id
