24e79d876e7e273cf1dcdfa87ce330dada755f75
phongyanon <phongyanon@gmail.com>
2017-07-27 18:07:48 +0700

    [ADD] customer refund report

---
+++ specific/cmo_customer_refund_report/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "CMO :: Customer Refund Report Jasper (PDF)",
+    'summary': "",
+    'author': "Phongyanon Y.",
+    'website': "http://ecosoft.co.th",
+    'category': 'Tools',
+    "version": "1.0",
+    'depends': [
+        'report',
+        'cmo_account',
+    ],
+    'data': [
+        'report/report_refund.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
