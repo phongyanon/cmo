87285e3f1b6b2aac113c9f64700a458a45dcde81
phongyanon <phongyanon@gmail.com>
2017-08-28 16:43:34 +0700

    [REF] self._context.get('new_sale_revision', False)

--- specific/cmo_sale_order_revision_enhance/models/sale_order.py
+++ specific/cmo_sale_order_revision_enhance/models/sale_order.py
@@ -76,7 +76,7 @@ class sale_order(models.Model):
         if not defaults:
             defaults = {}
         for order in self:
-            if order.env.context.get('new_sale_revision'):
+            if self._context.get('new_sale_revision', False):
                 prev_name = order.name
                 if order.current_revision_id:
                     current_order = order.env['sale.order'].browse(
@@ -111,7 +111,7 @@ class sale_order(models.Model):
                 }
                 defaults.update(context_update)
         res = super(sale_order, self).copy(defaults)
-        if order.env.context.get('new_sale_revision'):
+        if self._context.get('new_sale_revision', False):
             res.write({'active': False})
         return res
 
