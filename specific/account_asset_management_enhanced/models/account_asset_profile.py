e4b82b678676feed99d653a0333bcd19ece83398
phongyanon <phongyanon@gmail.com>
2017-11-14 16:18:55 +0700

    [IMP] change sequence when change asset account

--- specific/account_asset_management_enhanced/models/account_asset_profile.py
+++ specific/account_asset_management_enhanced/models/account_asset_profile.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from openerp import fields, models, api
+from openerp import fields, models, api, _
+from openerp.exceptions import ValidationError
 
 
 class AccountAssetProfile(models.Model):
@@ -34,4 +35,26 @@ class AccountAssetProfile(models.Model):
                 new_sequence = self._create_sequence_asset(account_id)
                 vals['sequence_id'] = new_sequence.id
         res = super(AccountAssetProfile, self).create(vals)
+        return res
+
+    @api.multi
+    def write(self, vals):
+        res = super(AccountAssetProfile, self).write(vals)
+        account_asset_id = vals.get('account_asset_id')
+        for profile in self:
+            is_running = bool(profile.sequence_id.number_next_actual > 1)
+            if is_running:
+                raise ValidationError(
+                    _('Sequence in this asset profile was running.'))
+            elif account_asset_id:
+                account_id = self.env['account.account'].browse(
+                    account_asset_id)
+                sequence_id = self.env['ir.sequence'].search([
+                    ('prefix', '=', account_id.code + '-'),
+                ])
+                if sequence_id and account_id:
+                    profile.write({'sequence_id': sequence_id[0].id})
+                elif account_id:
+                    new_sequence = profile._create_sequence_asset(account_id)
+                    profile.write({'sequence_id': new_sequence.id})
         return res
